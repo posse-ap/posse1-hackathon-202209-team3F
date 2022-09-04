8fc42a524a144ac5a343fa9867d5ad7771292f78
inose-yuudai <inobee1412@yahoo.co.jp>
2022-09-04 23:42:22 +0900

    お知らせを表示

--- assets/js/birthday.js
+++ assets/js/birthday.js
@@ -1,75 +1,99 @@
 "use strict"
 
-const picture7Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
+
+
+const picture1Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
 let num = -1;
 
-function slideShow() {
+function slideShow1() {
     if (num === 1) {
         num = 0;
     } else {
         num++;
     }
-    document.getElementById("slideImg7").src = picture7Array[num];
+    document.getElementById("slideImg1").src = picture1Array[num];
 }
-setInterval(slideShow, 500);
+setInterval(slideShow1, 500);
+
+const picture2Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
+
+
+let num2 = -1;
 
 
 
-
-const picture8Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
-
-
-let num8 = -1;
-
-
-
-function slideShow8() {
+function slideShow2() {
     if (num === 1) {
-        num8 = 0;
+        num2 = 0;
     } else {
-        num8++;
+        num2++;
     }
-    document.getElementById("slideImg8").src = picture8Array[num8];
+    document.getElementById("slideImg2").src = picture2Array[num2];
 }
 
 
-setInterval(slideShow8, 500);
+setInterval(slideShow2, 500);
 
-const picture9Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
-
-
-let num9 = -1;
+const picture3Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
 
 
+let num3 = -1;
 
-function slideShow9() {
+function slideShow3() {
+    if (num3 === 1) {
+        num3 = 0;
+    } else {
+        num3++;
+    }
+    document.getElementById("slideImg3").src = picture3Array[num3];
+}
+
+
+setInterval(slideShow3, 300);
+
+const picture4Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png'];
+
+
+let num4 = -1;
+
+function slideShow4() {
     if (num === 1) {
-        num9 = 0;
+        num4 = 0;
     } else {
-        num9++;
+        num4++;
     }
-    document.getElementById("slideImg9").src = picture9Array[num9];
+    document.getElementById("slideImg4").src = picture4Array[num4];
 }
 
-
-setInterval(slideShow9, 500);
-
-
-// const picture9Array = ["assets/img/birthday-itsuki.jpeg", 'assets/img/posseロゴ-removebg-preview.png', "assets/img/birthday-itsuki.jpeg", ];
+setInterval(slideShow3, 500);
 
 
-// let num9 = -1;
 
-// function slideShow9() {
-//     if (num === 2) {
-//         num9 = 0;
-//     } else {
-//         num9++;
+
+
+let ALL_YEAR = []
+const birthdayDate =
+    document.getElementById(`birthdaydate`);
+
+
+// for (let i = 4; i < 0; i--) {
+//     const tab =
+//         document.getElementById(`tab`);
+//     tab.innerHTML = `< li > < a href = "
+//     #202${i- 1}" id= "year202${i- 1} class="
+//     main - year "></a></li>`
+//     for (let j = 0; j < 4; j++) {
+//         birthdayDate.innerHTML = ` < ul class = "main-box-inner" > `
+//         ` < li class = "main-month-box" > `
+//         ` < button class = "main-month" > $ {
+//         [j + 1] } + "月" < /button>`
+//         ` <figure class="main-month-img">
+//         <img src="assets/img/posseロゴ-removebg-preview.png" width="100%" id="slideimg${i+7}">
+//     </figure>`
+
+
 //     }
-//     document.getElementById("slideImg9").src = picture9Array[num9];
 // }
-
-// setInterval(slideShow9, 300);
 
 
 
@@ -98,26 +122,7 @@ z.appendChild(text4);
 
 
 
-const ALL_YEAR = [{
-        id: 1,
-        when: "2022",
-        answers: ["約28万人", "約79万人", "約183万人"],
-        correctNumber: 1,
-        href: "経済産業省 2019年3月 － IT 人材需給に関する調査",
-    },
-    {
-        id: 2,
-        when: '2021',
-        answers: ['INTECH', 'BIZZTECH', 'X-TECH'],
-        correctNumber: 2,
-    },
-    {
-        id: 3,
-        when: '2020',
-        answers: ['Internet of Things', 'Integrate into Technology', 'Information on Tool'],
-        correctNumber: 0,
-    },
-];
+
 // $('.main-box-inner').slick({
 //     autoplay: true, //自動的に動き出すか。初期値はfalse。
 //     infinite: true, //スライドをループさせるかどうか。初期値はtrue。
@@ -148,3 +153,144 @@ const ALL_YEAR = [{
 //         counter = 1;
 //     }
 // }
+
+
+function GethashID(hashIDName) {
+    if (hashIDName) {
+        //タブ設定
+        $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+            var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得
+            if (idName == hashIDName) { //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+                var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+                $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+                $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+                //表示させるエリア設定
+                $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+                $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
+            }
+        });
+    }
+}
+
+//タブをクリックしたら
+$('.tab a').on('click', function() {
+    var idName = $(this).attr('href'); //タブ内のリンク名を取得
+    GethashID(idName); //設定したタブの読み込みと
+    return false; //aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function() {
+    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+    var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+    GethashID(hashName); //設定したタブの読み込み
+    document.getElementById("year2022")
+});
+
+
+
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+// 月末だとずれる可能性があるため、1日固定で取得
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
+
+// 初期表示
+window.onload = function() {
+    showProcess(today, calendar);
+};
+// 前の月表示
+function prev() {
+    showDate.setMonth(showDate.getMonth() - 1);
+    showProcess(showDate);
+}
+
+// 次の月表示
+function next() {
+    showDate.setMonth(showDate.getMonth() + 1);
+    showProcess(showDate);
+}
+
+// カレンダー表示
+function showProcess(date) {
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    document.querySelector('#header').innerHTML = year + "年 " + (month + 1) + "月";
+
+    var calendar = createProcess(year, month);
+    document.querySelector('#calendar').innerHTML = calendar;
+}
+
+// カレンダー作成
+function createProcess(year, month) {
+    // 曜日
+    var calendar = "<table><tr class='dayOfWeek'>";
+    for (var i = 0; i < week.length; i++) {
+        calendar += "<th>" + week[i] + "</th>";
+    }
+    calendar += "</tr>";
+
+    var count = 0;
+    var startDayOfWeek = new Date(year, month, 1).getDay();
+    var endDate = new Date(year, month + 1, 0).getDate();
+    var lastMonthEndDate = new Date(year, month, 0).getDate();
+    var row = Math.ceil((startDayOfWeek + endDate) / week.length);
+
+    // 1行ずつ設定
+    for (var i = 0; i < row; i++) {
+        calendar += "<tr>";
+        // 1colum単位で設定
+        for (var j = 0; j < week.length; j++) {
+            if (i == 0 && j < startDayOfWeek) {
+                // 1行目で1日まで先月の日付を設定
+                calendar += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
+            } else if (count >= endDate) {
+                // 最終行で最終日以降、翌月の日付を設定
+                count++;
+                calendar += "<td class='disabled'>" + (count - endDate) + "</td>";
+            } else {
+                // 当月の日付を曜日に照らし合わせて設定
+                count++;
+                if (year == today.getFullYear() &&
+                    month == (today.getMonth()) &&
+                    count == today.getDate()) {
+                    calendar += "<td class='today'>" + count + "</td>";
+                } else {
+                    calendar += "<td>" + count + "</td>";
+                }
+            }
+        }
+        calendar += "</tr>";
+    }
+    return calendar;
+}
+
+
+
+const buttonOpen = document.getElementById('modalOpen');
+const modal = document.getElementById('easyModal');
+const buttonClose = document.getElementsByClassName('modalClose')[0];
+
+// ボタンがクリックされた時
+buttonOpen.addEventListener('click', modalOpen);
+
+function modalOpen() {
+    modal.style.display = 'block';
+}
+
+// バツ印がクリックされた時
+buttonClose.addEventListener('click', modalClose);
+
+function modalClose() {
+    modal.style.display = 'none';
+}
+
+// モーダルコンテンツ以外がクリックされた時
+addEventListener('click', outsideClose);
+
+function outsideClose(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
